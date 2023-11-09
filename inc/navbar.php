2cc0bb675f66596fe1ce503160d9f2d911aed8a2
unknown <yopannalpiss@gmail.com>
2023-11-10 06:55:53 +0700

    FE index

---
+++ inc/navbar.php
@@ -0,0 +1,22 @@
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Signika:wght@300;400;500&display=swap" rel="stylesheet">
+</head>
+<nav>
+    <div class="navLeft">
+        <a href="index.php">
+            <img src="assets/images/ado.jpeg" alt="">
+            <h2>Spotipi</h2>
+        </a>
+        <a href="playlist.php">
+            <h2>Playlist</h2>
+        </a>
+    </div>
+    <div class="navMid">
+        <input type="text" name="" id="">
+    </div>
+    <div class="navRight">
+        <a href="logout.php">Log Out</a>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 include "inc/functions.php";
+include "inc/navbar.php";
 
 $song = query("SELECT * FROM songs");
 
-if(!isset($_SESSION['login'])) {
+if (!isset($_SESSION['login'])) {
     header("Location: login.php");
     exit;
 }
@@ -12,35 +13,42 @@ if(!isset($_SESSION['login'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/global.css">
     <title>Index Brok</title>
 </head>
+
 <body>
-    <nav>
-        <div class="navLeft">
-            <a href="index.php">
-            <img src="" alt="">
-            <h2>Spotipi</h2>
-            </a>
-            <a href="playlist.php">Playlist</a>
-        </div>
-        <div class="navMid">
-            <input type="text" name="" id="">
-        </div>
-        <div class="navRight">
-            <a href="logout.php">Log Out</a>
-        </div>
-    </nav>
+
     <h1>Index Brok</h1>
     <a href="addSong.php">Add More Song</a> <br><br>
-    <?php foreach($song as $row) : ?>
-    <li>
-        <a href="updateSong.php?id=<?= $row['id']; ?>"><?= $row['title']; ?></a>
-    </li>
-    <?php endforeach; ?>
-    
+    <div class="songList">
+        <?php foreach ($song as $row): ?>
+            <!-- <li>
+            <a href="updateSong.php?id=<?= $row['id']; ?>">
+                <?= $row['title']; ?>
+            </a>
+            </li> -->
+            <div class="songBox">
+                <div>
+                    <div class="photo">
+                        <img src="assets/upload/images/<?= $row['photo'] ?>" alt="">
+                        <a href="updateSong.php?id=<?= $row['id']; ?>" class="edit">edit</a>
+                    </div>
+
+                    <h4>
+                        <?= $row['title']; ?>
+                    </h4>
+                    <h5>
+                        <?= $row['artist']; ?>
+                    </h5>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
+
 </html>
