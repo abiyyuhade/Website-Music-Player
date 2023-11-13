d89e94e6a62c3a31c556f62672cd402a49bbd985
unknown <yopannalpiss@gmail.com>
2023-11-14 00:14:28 +0700

    FE song box update

--- index.php
+++ index.php
@@ -23,7 +23,9 @@ if (isset($_POST['findSong'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/global.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Index Brok</title>
     <script>
         var currentPlayingMusicId = null;
@@ -35,10 +37,10 @@ if (isset($_POST['findSong'])) {
             if (currentPlayingMusicId === musicId) {
                 if (audioElement.paused) {
                     audioElement.play();
-                    playButton.innerText = 'Pause';
+                    // playButton.innerText = 'Pause';
                 } else {
                     audioElement.pause();
-                    playButton.innerText = 'Play';
+                    // playButton.innerText = 'Play';
                 }
             } else {
                 var currentPlayingAudio = document.getElementById('music' + currentPlayingMusicId);
@@ -46,15 +48,31 @@ if (isset($_POST['findSong'])) {
                     currentPlayingAudio.pause();
                     var currentPlayingButton = document.getElementById('playPauseButton_' + currentPlayingMusicId);
                     if (currentPlayingButton) {
-                        currentPlayingButton.innerText = 'Play';
+                        // currentPlayingButton.innerText = 'Play';
                     }
                 }
 
                 audioElement.play();
                 currentPlayingMusicId = musicId;
-                playButton.innerText = 'Pause';
+                // playButton.innerText = 'Pause';
             }
         }
+
+        function toggleDropdown(id) {
+            const dropdownContent = document.getElementById('dropdown_' + id);
+            dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+        }
+
+        // Menutup dropdown saat pengguna mengklik di luar dropdown
+        window.onclick = function (event) {
+            const photos = document.querySelectorAll('.photo');
+            photos.forEach(function (photo) {
+                const dropdownContent = photo.querySelector('.dropdown');
+                if (event.target !== photo && !photo.contains(event.target)) {
+                    dropdownContent.style.display = 'none';
+                }
+            });
+        };
     </script>
 
 </head>
@@ -62,7 +80,7 @@ if (isset($_POST['findSong'])) {
 <body>
     <button class="addSong"><a href="addSong.php">Add Song</a></button>
     <div class="songList">
-        <?php foreach ($song as $row) : ?>
+        <?php foreach ($song as $row): ?>
             <!-- <li>
             <a href="updateSong.php?id=<?= $row['id']; ?>">
                 <?= $row['title']; ?>
@@ -71,21 +89,41 @@ if (isset($_POST['findSong'])) {
             <div class="songBox">
                 <div>
                     <div class="photo">
+                        <button onclick="toggleDropdown('<?= $row['id']; ?>')">
+                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                        </button>
                         <img src="assets/upload/images/<?= $row['photo'] ?>" alt="">
-                        <a href="updateSong.php?id=<?= $row['id']; ?>" class="edit">edit</a>
-                        <a href="deleteSong.php?id=<?= $row['id']; ?>" class="delete">delete</a>
-                        <a href="javascript:void(0);" onclick="toggleMusic('<?= $row['id']; ?>')" id="playPauseButton_<?= $row['id']; ?>">Play</a>
+                        <div id="dropdown_<?= $row['id']; ?>" class="dropdown">
+                            <a href="updateSong.php?id=<?= $row['id']; ?>" class="edit">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                                edit
+                            </a>
+                            <a href="deleteSong.php?id=<?= $row['id']; ?>" class="delete">
+                                <i class="fa-solid fa-trash"></i>
+                                delete
+                            </a>
+                        </div>
                     </div>
-                    <?php
-                    $musicPath = './assets/upload/music/' . $row['music'];
-                    ?>
-                    <audio id="music<?= $row['id']; ?>" src="<?= $musicPath; ?>"></audio>
-                    <h4>
-                        <?= $row['title']; ?>
-                    </h4>
-                    <h5>
-                        <?= $row['artist']; ?>
-                    </h5>
+                    <div class="titlePlayBox">
+                        <div class="titleBox">
+                            <h4>
+                                <?= $row['title']; ?>
+                            </h4>
+                            <h5>
+                                <?= $row['artist']; ?>
+                            </h5>
+                        </div>
+                        <div class="playBox">
+                            <?php
+                            $musicPath = './assets/upload/music/' . $row['music'];
+                            ?>
+                            <audio id="music<?= $row['id']; ?>" src="<?= $musicPath; ?>"></audio>
+                            <a href="javascript:void(0);" onclick="toggleMusic('<?= $row['id']; ?>')"
+                                id="playPauseButton_<?= $row['id']; ?>" class="playButton">
+                                <i class="fa-solid fa-play"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         <?php endforeach; ?>
